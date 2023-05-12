aee0de2a1ff7589637c2a74413d5329e0f8b8195
MarcusKyung <Kyungmj@gmail.com>
2023-05-12 08:49:49 -0700

    test pass for order constructor

---
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
